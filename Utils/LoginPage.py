5d4098161a7c294169355c8eec4af1e40c94d957
Ivana Papaño <ivana.papanio@gmail.com>
2026-05-11 18:06:33 -0300

    Refactorización de Login con esperas explícitas

--- Utils/LoginPage.py
+++ Utils/LoginPage.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
     def __init__(self, driver):
@@ -8,6 +10,7 @@ class LoginPage:
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
 
+    # Métodos para interactuar con la página
     def enter_username(self, username):
         self.driver.find_element(*self.username_field).send_keys(username)
 
@@ -16,4 +19,22 @@ class LoginPage:
 
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
+
+    
+    def login_to_sauce(self, username, password):
+        # 1. Espera explícita para asegurarnos de que la página está lista
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.presence_of_element_located(self.username_field))
         
+        # 2. Usamos tus métodos
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
+        
+        # 3. Validación de que entramos 
+        wait.until(EC.url_contains("/inventory.html"))
+
+
+def login(driver):
+    login_page = LoginPage(driver)
+    login_page.login_to_sauce("standard_user", "secret_sauce")
